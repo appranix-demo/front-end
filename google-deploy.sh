a127e242ee24deb847158532e23fddd472264309
Rushinth John <rushinth@appranix.com>
2019-07-13 15:13:58 +0530

    Updated code for Codeship - CPS demo

---
+++ google-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Use the codeship_gce_service to authenticate
+echo "Authenticating"
+codeship_google authenticate
+
+# gcloud default values
+gcloud config set compute/zone us-east4-a
+gcloud config set project project--3443695228107877164
+
+# connect to the cluster running the app
+gcloud container clusters get-credentials apnx-zonal-test-cluster --zone us-east4-a --project project--3443695228107877164
+
+# Deploying the image
+kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:ax-shop
+# kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:0.3.12
