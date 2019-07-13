d5169f9e15f310b0f6591ddbc55793257e6f927c
Rushinth John <rushinth@appranix.com>
2019-07-13 18:27:19 +0530

    Testing Appranix CPS

--- appranix_cps.sh
+++ appranix_cps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -XPOST -H 'Accept: application/json' -H "Content-type: application/json" -d '{"protectionPlanId": "21556513:1954153:6a74bbe3-07cd-4c09-80d3-6b7b9f7699fe", "policyName": "ON_DEMAND", "protectionPolicyId": "ON_DEMAND_POLICY_ID", "accountId": 21556513, "organizationId": 1954153,"clusterName": "cps-demo", "jobType": "BACKUP", "scheduleType": "MANUAL"}' 'https://qa.appranix.net/cps-api/api/v1/job'
+curl -XPOST -H 'Accept: application/json' -H "Content-type: application/json" -d '{"protectionPlanId": "21556513:1954153:6a74bbe3-07cd-4c09-80d3-6b7b9f7699fe", "policyName": "ON_DEMAND", "protectionPolicyId": "ON_DEMAND_POLICY_ID", "accountId": 21556513, "organizationId": 1954153,"clusterName": "cps-demo", "jobType": "BACKUP", "scheduleType": "MANUAL"}' 'https://qa-api.appranix.net/cps-api/api/v1/job'
 sleep 10
