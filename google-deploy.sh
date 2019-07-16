e12e824597f0cae6cbbef2e1939fe88d81b40574
Appranix-Ops-Team <ops-team@appranix.com>
2019-07-16 12:22:57 +0530

    color and cluster change

--- google-deploy.sh
+++ google-deploy.sh
@@ -9,7 +9,8 @@ gcloud config set compute/zone us-central1-a
 gcloud config set project appranix-managed-dev-01
 
 # connect to the cluster running the app
-gcloud container clusters get-credentials qa-cps-test-cluster --zone us-central1-a --project appranix-managed-dev-01
+#gcloud container clusters get-credentials qa-cps-test-cluster --zone us-central1-a --project appranix-managed-dev-01
+gcloud beta container clusters get-credentials gke-demo-cluster-eus --region us-east1 --project appranix-managed-dev-01
 
 # Deploying the image
 kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:ax-shop
