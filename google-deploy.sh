8292987e2afcdc88ef58a2be4b56377a5c57956e
Rushinth John <rushinth@appranix.com>
2019-07-16 14:40:35 +0530

    Fixed bugs

--- google-deploy.sh
+++ google-deploy.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
-echo $GOOGLE_AUTH_JSON
-echo $GOOGLE_AUTH_EMAIL
-echo $GOOGLE_PROJECT_ID
-cat /usr/bin/codeship_google
-
-# gcloud default values
-gcloud config set account ax-mng-dev-cloudsql-test@appranix-managed-dev-01.iam.gserviceaccount.com
-gcloud config set project appranix-managed-dev-01
-
-echo $GOOGLE_AUTH_JSON
-echo $GOOGLE_AUTH_EMAIL
-echo $GOOGLE_PROJECT_ID
 
 # Use the codeship_gce_service to authenticate
 echo "Authenticating"
 codeship_google authenticate
 
+# gcloud default values
+gcloud config set project appranix-managed-dev-01
+
 # connect to the cluster running the app
-#gcloud container clusters get-credentials qa-cps-test-cluster --zone us-central1-a --project appranix-managed-dev-01
 gcloud container clusters get-credentials gke-demo-cluster-eus --region us-east1 --project appranix-managed-dev-01
 
 # Deploying the image
