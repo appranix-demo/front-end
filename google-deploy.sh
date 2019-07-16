68568c7bee0bebd4dfde199f1c9065ca163adb8a
Rushinth John <rushinth@appranix.com>
2019-07-16 15:46:21 +0530

    Final Test

--- google-deploy.sh
+++ google-deploy.sh
@@ -13,9 +13,3 @@ gcloud container clusters get-credentials gke-demo-cluster-eus --region us-east1
 # Deploying the image
 kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:$CI_TIMESTAMP
 # kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:0.3.12
-
-
-echo $CI_TIMESTAMP
-echo ${CI_TIMESTAMP}
-echo "${CI_TIMESTAMP}"
-echo "$CI_TIMESTAMP"
