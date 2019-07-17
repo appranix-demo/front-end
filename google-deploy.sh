dc63a3d7ed8aee2e87016a4dae9ef75bcff9dace
Rushinth John <rushinth@appranix.com>
2019-07-17 17:13:47 +0530

    Reset color

--- google-deploy.sh
+++ google-deploy.sh
@@ -14,3 +14,6 @@ gcloud container clusters get-credentials gke-demo-cluster-eus --region us-east1
 # Deploying the image
 kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:$CI_TIMESTAMP
 # kubectl -n sock-shop set image deployment/front-end front-end=gcr.io/appranix-public-cr/sock-shop/front-end:0.3.12
+
+# Starting disaster
+bash deploy/google-deploy.sh
